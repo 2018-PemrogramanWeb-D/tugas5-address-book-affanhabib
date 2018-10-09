9d964473d80a67cd901075ce37c07302c2266b42
Affan Ahsanul Habib <affanahsan02@gmail.com>
2018-10-09 22:37:52 +0700

    Tugas 5

---
+++ edit.php
@@ -0,0 +1,78 @@
+<?php
+// include database connection file
+include_once("config.php");
+ 
+// Check if form is submitted for user update, then redirect to homepage after update
+if(isset($_POST['update']))
+{	
+	$id = $_POST['id'];
+	
+	$name=$_POST['name'];
+	$nrp=$_POST['nrp'];
+	$address=$_POST['address'];
+	$mobile=$_POST['mobile'];
+	$email=$_POST['email'];
+		
+	// update user data
+	$result = mysqli_query($mysqli, "UPDATE users SET name='$name',nrp='$nrp',email='$email',mobile='$mobile' WHERE id=$id");
+	
+	// Redirect to homepage to display updated user in list
+	header("Location: index.php");
+}
+?>
+<?php
+// Display selected user data based on id
+// Getting id from url
+$id = $_GET['id'];
+ 
+// Fetech user data based on id
+$result = mysqli_query($mysqli, "SELECT * FROM users WHERE id=$id");
+ 
+while($user_data = mysqli_fetch_array($result))
+{
+	$name = $user_data['name'];
+	$nrp = $user_data['nrp'];
+	$address = $user_data['address'];
+	$email = $user_data['email'];
+	$mobile = $user_data['mobile'];
+}
+?>
+<html>
+<head>	
+	<title>Edit User Data</title>
+</head>
+ 
+<body>
+	<a href="index.php">Home</a>
+	<br/><br/>
+	
+	<form name="update_user" method="post" action="edit.php">
+		<table border="0">
+			<tr> 
+				<td>Name</td>
+				<td><input type="text" name="name" value=<?php echo $name;?>></td>
+			</tr>
+			<tr>
+				<td>NRP</td>
+				<td><input type="text" name="nrp" value=<?php echo $nrp;?>></td>
+			</tr>
+			<tr>
+				<td>Address</td>
+				<td><input type="text" name="address" value=<?php echo $address;?>></td>
+			</tr>
+			<tr> 
+				<td>Email</td>
+				<td><input type="text" name="email" value=<?php echo $email;?>></td>
+			</tr>
+			<tr> 
+				<td>Mobile</td>
+				<td><input type="text" name="mobile" value=<?php echo $mobile;?>></td>
+			</tr>
+			<tr>
+				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
+				<td><input type="submit" name="update" value="Update"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
